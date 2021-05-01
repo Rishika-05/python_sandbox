a8248c03d9ec038652d53da0f2a1f543b4794a4c
Rishika <rishikaraj7263@gmail.com>
2021-05-01 19:15:03 +0530

    add

--- python_sandbox_starter/dictionaries.py
+++ python_sandbox_starter/dictionaries.py
@@ -1,2 +1,49 @@
 # A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
 # Read more about dictionaries at https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+
+# create dict
+person = {
+    'first_name': 'Rish',
+    'last_name': 'Raj',
+    'age':18
+}
+
+# constructor 
+# person2 = dict(first_name='Rish',last_name='Raj')
+
+
+# get value
+print(person['first_name'])
+print(person.get('last_name'))
+
+# add key/value
+person['phone']='555-555-5555'
+
+# get dict keys
+print(person.keys())
+
+# get dict items
+print(person.items())
+
+# copy dict
+person2 = person.copy()
+person2['city']='delhi'
+
+# remove item
+del(person['age'])
+person.pop('phone')
+
+# clear
+person.clear()
+
+# get length
+print(len(person2))
+
+# list of dict
+people = [
+    {'name':'martha','age':30},
+    {'name':'kevin','age':25}
+]
+
+print(people[1]['name'])
+
--- python_sandbox_starter/functions.py
+++ python_sandbox_starter/functions.py
@@ -1,5 +1,10 @@
 # A function is a block of code which only runs when it is called. In Python, we do not use parentheses and curly brackets, we use indentation with tabs or spaces
 
+# create function
+def sayHello(name):
+    print(f'Hello {name}')
+
+sayHello('Rish')
 
 
 # A lambda function is a small anonymous function.
